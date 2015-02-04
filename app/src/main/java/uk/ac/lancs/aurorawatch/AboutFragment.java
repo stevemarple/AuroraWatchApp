ce5c47e8af7aabd416a23117e22641fd52862cc1
James Billings <james@puffle.co.uk>
2015-02-04 21:10:13 +0000

    Add 'about' fragment and class

---
+++ app/src/main/java/uk/ac/lancs/aurorawatch/AboutFragment.java
@@ -0,0 +1,19 @@
+package uk.ac.lancs.aurorawatch;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by jamesb on 04/02/2015.
+ */
+public class AboutFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.about, container, false);
+    }
+}
