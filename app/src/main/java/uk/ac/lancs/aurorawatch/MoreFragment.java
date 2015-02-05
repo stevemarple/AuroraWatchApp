6a066241bb99d3d9e363a759d4cb361095381202
James Billings <james@puffle.co.uk>
2015-02-05 17:44:57 +0000

    refactor the 'about' fragment to be the 'more' fragment, and add buttons for website and FAQ's. Remove licensing link

--- app/src/main/java/uk/ac/lancs/aurorawatch/MoreFragment.java
+++ app/src/main/java/uk/ac/lancs/aurorawatch/MoreFragment.java
@@ -9,11 +9,11 @@ import android.view.ViewGroup;
 /**
  * Created by jamesb on 04/02/2015.
  */
-public class AboutFragment extends Fragment {
+public class MoreFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.about, container, false);
+        return inflater.inflate(R.layout.more, container, false);
     }
 }
